c639d4222469ea8f5535e83b0d4b3f6e353383d6
Amelia Jastremski <amelia.jastremski@gmail.com>
2025-10-22 14:42:12 +0300

    farm snake

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
-// let gooPosition = find_monster('goo');
-// let goResult = smart_move(gooPosition);
-
-// if (goResult == true) {
-//     let target = target_monster('goo');
-//     if (target) {
-//         let attackResult = attack('goo');
-//     }
-// }
-
 setInterval(routine_move, 1000/4);
 setInterval(routine_attack, 1000/4);
 setInterval(routine, 1000/4);
 
+let farm_monsters = ["snake", "osnake"];
+
 function routine_move() {
     // todo : if mpot count == 0 || hpot count == 0 then go to town
     // todo : if near no required monsters and hpot count == 9999 and mpot count == 9999 then go to hunting area
-
-    let target = get_targeted_monster();
-    if (target)  {
-        if (!is_in_range(target))
-        {
-            move(
-                character.x+(target.x-character.x)/2,
-                character.y+(target.y-character.y)/2
-                );
-            // Walk half the distance
+    if (!character.moving && !smart.moving) {  
+        if (get_near_monsters_count(farm_monsters) == 0) {
+            smart_move(farm_monsters[0]);
+        }
+        else {
+            let target = get_targeted_monster();
+            if (target)  {
+                if (!is_in_range(target))
+                {
+                    move(
+                        character.x+(target.x-character.x)/2,
+                        character.y+(target.y-character.y)/2
+                        );
+                    // Walk half the distance
+                }
+            }
         }
     }
 }
@@ -35,7 +33,9 @@ function routine_attack() {
     }
     else {
         // todo : if mpot count == 0 || hpot count == 0 then do not attack monsters
-
+        if (get_near_monsters_count(farm_monsters) == 0) {
+            return;
+        }
 
         let target = get_targeted_monster();
         
@@ -59,6 +59,13 @@ function routine_attack() {
 
 function routine() {
     if (!character.rip) {
+        let hpot_count = inventory_item_count("hpot1");
+        let mpot_count = inventory_item_count("mpot1");
+
+        set_message("" + hpot_count + " " + mpot_count);
+
+        merge_inventory_items();
+
         // todo : if in town and hpot count < 9999 || mpot count < 9999 then buy pots
 
         let current_mp = character.mp;
@@ -81,4 +88,79 @@ function routine() {
         // use_hp_or_mp();
         loot();
     }
+}
+
+function inventory_item_count(item_name) {
+    let result = 0;
+    for (let i = 0; i < 42; i++) {
+        let item = character.items[i];
+        if (item != null && item.name === item_name) {
+            if (!item.q) {
+                result += 1;
+            } else {
+                result += item.q;
+            }
+        }
+    }
+    return result;
+}
+
+function sum_to_numbers(number1, number2) {
+    let result = number1 + number2;
+    return result;
+}
+
+function merge_inventory_items() {
+    for (let i = 0; i < 42; i++) {
+        let item = character.items[i];
+        if (item && item.name == "hpot1" && i != 41) {
+            swap(41, i);
+        }
+
+        if (item && item.name == "mpot1" && i != 40) {
+            swap(40, i);
+        }
+    }
+
+    for (let i = 0; i < 42; i++) {
+        let item = character.items[i];
+        if (item && item.q) {
+            let same_items = get_inventory_item_indexes(item.name);
+            let operations_count = same_items.length - 1;
+            if (operations_count > 0) {
+                for (let j = operations_count; j > 0; j--) {
+                    swap(same_items[j], same_items[j-1]);
+                }
+            }
+        }
+    }
+}
+
+function get_inventory_item_indexes(item_name) {
+    let indexes = [];
+    for (let i = 0; i < 42; i++) {
+        let item = character.items[i];
+        if (item != null && item.name === item_name) {
+            indexes.push(i);
+        }
+    }
+    return indexes;
+}
+
+function get_near_monsters_count(mtypes) {
+    let r = character.range + 400;
+    var result = {};
+    for (id in parent.entities)
+    {
+        var entity = parent.entities[id];
+
+        if (entity.mtype != null && mtypes.includes(entity.mtype) && parent.G.monsters[entity.mtype])
+        {
+            if (distance(entity, character) < r) {
+                result[id] = entity;
+            }
+        }
+    }
+
+    return Object.keys(result).length;
 }
